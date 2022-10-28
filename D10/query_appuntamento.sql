5fdd824a48abd68109eb18169563fbdb352dcb0b
Roberto Capancioni <roberto.capancioni@gmail.com>
2022-10-28 11:11:29 +0200

    Create query_appuntamento.sql

---
+++ D10/query_appuntamento.sql
@@ -0,0 +1,14 @@
+select 
+      a.ID,
+      a.DATA_INIZIO,
+      a.DATA_FINE,
+      a.DESCRIZIONE,
+      a.TIPO_APPUNTAMENTO_ID,
+     ta.TIPO_APPUNTAMENTO, 
+      a.NOME_ID,
+      n.NOME
+ from D10_APPUNTAMENTO a
+ join D10_TIPO_APPUNTAMENTO ta
+   on a.TIPO_APPUNTAMENTO_ID = ta.TIPO_APPUNTAMENTO_ID
+ join D10_NOME n
+   on a.NOME_ID = n.NOME_ID
