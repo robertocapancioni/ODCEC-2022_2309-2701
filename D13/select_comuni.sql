d6652350db6198ee8709a70a33c78989007ef908
Roberto Capancioni <roberto.capancioni@gmail.com>
2022-11-13 14:30:36 +0100

    Update select_comuni.sql

--- D13/select_comuni.sql
+++ D13/select_comuni.sql
@@ -13,3 +13,44 @@ delete
 delete 
   from d13_provincia 
  where provincia_cod in ('MN','PV','SO','VA')
+
+select c.comune,
+       c.provincia_cod,
+       p.provincia_cod,
+       p.provincia
+  from d13_comuni c
+  left
+  join d13_province p
+    on c.provincia_cod = p.provincia_cod
+
+select c.comune,
+       c.provincia_cod,
+       p.provincia_cod,
+       p.provincia
+  from d13_comuni c
+  left
+  join d13_province p
+    on c.provincia_cod = p.provincia_cod
+
+select c.comune,
+       c.provincia_cod,
+       p.provincia_cod,
+       p.provincia
+  from d13_comuni c
+ right
+  join d13_province p
+    on c.provincia_cod = p.provincia_cod
+
+select c.comune,
+       c.provincia_cod,
+       p.provincia_cod,
+       p.provincia
+  from d13_comuni c
+  full
+  join d13_province p
+    on c.provincia_cod = p.provincia_cod
+
+insert 
+  into d13_comuni 
+       (comune,provincia_cod)
+values ('Varese','MI')
