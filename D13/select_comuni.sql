1be27294e237195a1f8074bca12acbf785c3f327
Roberto Capancioni <roberto.capancioni@gmail.com>
2022-11-13 02:42:27 +0100

    Update select_comuni.sql

--- D13/select_comuni.sql
+++ D13/select_comuni.sql
@@ -6,3 +6,10 @@ select c.comune,
   join d13_province p
     on c.provincia_cod = p.provincia_cod
     
+delete 
+  from d13_comuni 
+ where provincia_cod in ('BG','BS','CO')
+
+delete 
+  from d13_provincia 
+ where provincia_cod in ('MN','PV','SO','VA')
