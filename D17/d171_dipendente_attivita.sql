01629377376f6b9aa6b0996fc86dde4c403a9404
Roberto Capancioni <roberto.capancioni@gmail.com>
2022-11-28 10:53:26 +0100

    Create d171_dipendente_attivita.sql

---
+++ D17/d171_dipendente_attivita.sql
@@ -0,0 +1,19 @@
+dipendente /insert 5
+  dipendente /nn /values francesca, marco, anna, simone, paola /unique
+  tariffa num /nn /values 200,150 
+
+cliente /insert 5
+  cliente vc50 /nn /values cliente_01,cliente_02,cliente_03,cliente_04,cliente_05 /unique
+
+commessa /insert 5
+  commessa vc30 /nn /values COMM01, COMM02, COMM03, COMM04, COMM05 /unique
+
+attivita /insert 200
+  dipendente_id /nn
+  cliente_id /nn
+  commessa_id vc100 /nn
+  data d /nn
+  ore num /nn
+
+#prefix: d171
+#ondelete: restrict
