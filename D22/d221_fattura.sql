994ffd5691264404f35f741ac268b89a365bb7ba
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-01-17 17:03:54 +0100

    Create d221_fattura.sql

---
+++ D22/d221_fattura.sql
@@ -0,0 +1,28 @@
+cliente
+  codice_cliente vc20 /nn
+  ragione_sociale vc50 /nn
+  piva vc11 /nn
+  indirizzo vc100 /nn
+  codice_sdi vc6 
+
+fattura_testata
+  anno num /nn
+  numero num /nn
+  descrizione vc50 /nn
+  data d /nn
+  cliente_id /nn
+
+fattura_dettaglio
+  fattura_testata_id /nn
+  riga num /nn
+  descrizione vc100 /nn
+  quantita num /nn
+  importo num /nn
+
+#prefix: d221
+#ondelete:restrict
+#drop:true
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Term
