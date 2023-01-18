3d2712475153afe299a2d4c247bb5c5c5c61ed9a
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-01-18 23:15:03 +0100

    Update D222_select.sql

--- D22/D222_select.sql
+++ D22/D222_select.sql
@@ -1,6 +1,35 @@
--- area annomese
+-- area annomese con filtro date
 select ANNOMESE, count(*) value
   from D222_VENDITA
  where DATA between nvl(:P2_DATA_INIZIO,data) and nvl(:P2_DATA_FINE,data)
  group by ANNOMESE
- order by ANNOMESE
+ order by ANNOMESE;
+ 
+-- area annomese con filtro date e tipo prodotto 
+SELECT ANNOMESE, COUNT(*) VALUE
+  FROM D222_VENDITA
+ WHERE DATA BETWEEN NVL(:P2_DATA_INIZIO,DATA) AND NVL(:P2_DATA_FINE,DATA)
+   AND TIPO_PRODOTTO = NVL(:P2_TIPO_PRODOTTO,TIPO_PRODOTTO)
+ GROUP BY ANNOMESE
+ ORDER BY ANNOMESE
+ 
+ -- area prodotto con filtro date
+select PRODOTTO, count(*) value
+  from D222_VENDITA
+ where DATA between nvl(:P2_DATA_INIZIO,data) and nvl(:P2_DATA_FINE,data)
+ group by PRODOTTO
+ order by PRODOTTO;
+ 
+-- area prodotto con filtro date e tipo prodotto 
+SELECT PRODOTTO, COUNT(*) VALUE
+  FROM D222_VENDITA
+ WHERE DATA BETWEEN NVL(:P2_DATA_INIZIO,DATA) AND NVL(:P2_DATA_FINE,DATA)
+   AND TIPO_PRODOTTO = NVL(:P2_TIPO_PRODOTTO,TIPO_PRODOTTO)
+ GROUP BY PRODOTTO
+ ORDER BY PRODOTTO
+ 
+ -- query per LOV (List of Values) Tipo Prodotto
+ select distinct tipo_prodotto d, 
+                tipo_prodotto r 
+           from d222_vendita
+          order by 1
