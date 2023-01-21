8bfde42f4d873dec03e7f6374fbb68e74ccc4a0d
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-01-21 16:39:14 +0100

    Create d231_drop.sql

---
+++ D23/d231_drop.sql
@@ -0,0 +1,6 @@
+drop view d231_fattura_testata_vw;
+drop view d231_fattura_dettaglio_vw;
+
+drop table d231_cliente cascade constraints;
+drop table d231_fattura_testata cascade constraints;
+drop table d231_fattura_dettaglio cascade constraints;
