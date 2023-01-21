334640086ec5069f03ba783c0c500b94f1dd7e26
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-01-21 11:12:55 +0100

    Update d231_views.sql

--- D23/d231_views.sql
+++ D23/d231_views.sql
@@ -13,4 +13,19 @@ select ft.id,
   from d231_fattura_testata ft
   join d231_cliente c on ft.cliente_id = c.id;
   
- 
+ create or replace view d231_fattura_dettaglio_vw as
+select fd.id,
+       fd.fattura_testata_id,
+       ft.anno,
+       ft.numero,
+       ft.data,
+        c.codice_cliente,
+        c.ragione_sociale,
+       fd.riga,
+       fd.descrizione,
+       fd.quantita,
+       fd.importo
+  from d231_fattura_dettaglio fd
+  join d231_fattura_testata ft on fd.fattura_testata_id = ft.id
+  join d231_cliente c on ft.cliente_id = c.id;
+
