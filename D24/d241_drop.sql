f05451c38c0e1f5572174a2b7eb57078edb61b0a
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-01-24 11:39:48 +0100

    Create d241_drop.sql

---
+++ D24/d241_drop.sql
@@ -0,0 +1,6 @@
+drop view d241_vendita_vw;
+
+drop table d241_cliente cascade constraints;
+drop table d241_tipo_prodotto cascade constraints;
+drop table d241_prodotto cascade constraints;
+drop table d241_vendita cascade constraints;
