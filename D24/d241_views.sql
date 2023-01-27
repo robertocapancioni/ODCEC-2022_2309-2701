1075bc6d6897a08e5c5dbf7d246cf0e5e413bcfe
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-01-27 08:48:20 +0100

    Update d241_views.sql

--- D24/d241_views.sql
+++ D24/d241_views.sql
@@ -14,20 +14,3 @@ select v.id,
   join d241_cliente c on v.cliente_id = c.id 
   join d241_prodotto p on v.prodotto_id = p.id
   join d241_tipo_prodotto tp on p.tipo_prodotto_id = tp.id;
-
-select v.id,
-       v.cliente,
-       v.zona_cliente,
-       v.prodotto,
-       v.tipo_prodotto,
-       v.data,
-       v.quantita,
-       v.importo
-  from d241_vendita_vw v
-  where exists (
-select 1
-  from apex_appl_acl_user_roles r
- where r.application_id = :app_id
-   and r.user_name = :app_user
-   and r.role_static_id in (v.zona_cliente,'ADMINISTRATOR')
- );
